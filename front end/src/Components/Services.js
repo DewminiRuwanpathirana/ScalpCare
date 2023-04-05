85f064bd0788d2dc73ea70456e51a5fe4a0324f1
Linara Wataraka <116620540+Linara2@users.noreply.github.com>
2023-04-05 10:36:29 +0530

    Update services.js
    update

--- front end/src/Components/Services.js
+++ front end/src/Components/Services.js
@@ -1,9 +1,9 @@
 //Services Page//
 import React, { useState } from 'react';
 import './Services.css';
-import Project1 from './images/identify_disease.jpg';
-import Project2 from './images/self_treatment.jpg';
-import Project3 from './images/dermatologist.jpg';
+import Project1 from './Images/identify_disease.jpg';
+import Project2 from './Images/self_treatment.jpg';
+import Project3 from './Images/dermatologist.jpg';
 
 function Project () {
     return (
